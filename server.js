184e0ea56da8b2f41d343d268fd3b2715b9d6ee5
javiermorales <javiertoto-00@hotmail.com>
2022-10-06 00:21:59 -0700

    password

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'M!tongasa1',
+        password: '',
         database: 'department_db'
     });
 
